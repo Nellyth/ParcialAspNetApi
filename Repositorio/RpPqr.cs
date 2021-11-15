0126a3e5d01882819732791a356b6b24b22d8c21
Nellyth Arroyo Marrugo <nellith25@gmail.com>
2021-11-15 16:35:08 -0500

    first commit

---
+++ Repositorio/RpPqr.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParcialAspNetApi.Models;
+using ParcialAspNetApi.Repositorio;
+
+namespace ParcialAspNetApi.Repositorio
+{
+	public class RpPqr
+	{
+		public User getUserOfRepository(int id)
+		{
+			RpUsers rpUser = new RpUsers();
+			return rpUser.getUser(id);
+		}
+
+		public static List<PQR> _listPqr = new List<PQR>()
+		{
+			new PQR()
+			{
+				idPqr=1,
+				grupoDeInteres="Ciudadano",
+				pais="Colombia",
+				departamento="Bolivar",
+				ciudad="Cartagena de Indias",
+				correo="nellith25@gmail.com",
+				medioDeNotificacionPara="email",
+				usuario=new RpUsers().getUser(1),
+				asunto="Test",
+				descripcion="Problema tecnicos"
+			},
+			new PQR()
+			{
+				idPqr=2,
+				grupoDeInteres="Empresa",
+				pais="1234567890",
+				departamento="Other Departament",
+				ciudad="Other City",
+				correo="asd@mailinator.com",
+				medioDeNotificacionPara="email",
+				usuario=new RpUsers().getUser(2),
+				asunto="Other neighorhood",
+				descripcion="Other neighorhood"
+			}
+		};
+
+		public IEnumerable<PQR> getPqrs()
+		{
+			return _listPqr;
+		}
+
+		public PQR getPqr(int id)
+		{
+			var pqr = _listPqr.Where(instance => instance.idPqr == id);
+
+			return pqr.FirstOrDefault();
+		}
+
+		public void Agregar(PQR newPqr)
+		{
+			_listPqr.Add(newPqr);
+		}
+
+		public void DeletePqr(PQR pqr)
+		{
+			_listPqr.Remove(pqr);
+		}
+	}
+}
---
+++ Repositorio/RpUsers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ParcialAspNetApi.Models;
+
+namespace ParcialAspNetApi.Repositorio
+{
+	public class RpUsers
+	{
+		public static List<User> _listUser = new List<User>()
+		{
+		   new User()
+			{
+				idUser=1,
+				tipoDocumento="CC",
+				documento="1044934746",
+				nombres="Nellyth J.",
+				apellidos="Arroyo Marrugo",
+				telefono="3005184942",
+				genero="Male",
+				direccion="Cartagena de Indias",
+				barrio="Los Calamares"
+			},
+			new User()
+			{
+				idUser=2,
+				tipoDocumento="CC",
+				documento="1234567890",
+				nombres="Other",
+				apellidos="User",
+				telefono="3158732176",
+				genero="Female",
+				direccion="Other direction",
+				barrio="other neighorhood"
+			}
+		};
+
+		public IEnumerable<User> getUsers()
+		{
+			return _listUser;
+		}
+
+		public User getUser(int id)
+		{
+			var user = _listUser.Where(instance => instance.idUser == id);
+
+			return user.FirstOrDefault();
+		}
+
+		public void Agregar(User newUser)
+		{
+			_listUser.Add(newUser);
+		}
+
+		public void DeleteUser(User user)
+		{
+			_listUser.Remove(user);
+		}
+	}
+}
